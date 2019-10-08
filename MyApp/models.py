d37dcd6ad8195ddc52792c45eb94777f6c9cc32b
adrian.michel <adrian.p.michel@hotmail.com>
2019-10-08 17:04:57 +0200

    Version 0.1

--- MyApp/models.py
+++ MyApp/models.py
@@ -1,3 +1,17 @@
+import datetime
+
 from django.db import models
 
 # Create your models here.
+
+class cronjob(models.Model):
+    titel = models.CharField(max_length=255)
+    adresse = models.URLField(max_length=255)
+    authentifizierung_checked = models.BooleanField(default=False)
+    benutzername = models.CharField(max_length=255)
+    passwort = models.CharField(max_length=255)
+    ausführen = models.DateTimeField(default=datetime.datetime.now())
+    benachrichtigung_fehlschlag = models.BooleanField(default=False)
+    benachrichtigung_erfolg = models.BooleanField(default=False)
+    benachrichtigung_deaktivierung = models.BooleanField(default=False)
+    antwort_speichern = models.BooleanField(default=False)
--- MyApp/views.py
+++ MyApp/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from . import models
+from .models import cronjob
 
 # Create your views here.
 
@@ -6,4 +8,24 @@ def index(request):
     return render(request, "index.html")
 
 def submit(request):
+    if(request.POST):
+        data = request.POST.dict()
+        titel = data.get("Titel")
+        adresse = data.get("Adresse")
+        authentifizierung_checked = data.get("UIcon")
+        benutzername = data.get("Benutzername")
+        passwort = data.get("Passwort")
+        ausführung = data.get("Ausführung")
+        benachrichtigung_fehlschlag = data.get("Benachrichtigungen_F")
+        benachrichtigung_erfolgnachfehl = data.get("Benachrichtigungen_E")
+        benachrichtigung_deaktivierung = data.get("Benachrichtigungen_D")
+        antwort_speichern = data.get("Antworten_speichern")
+
+
+
+
+
     return render(request, "index.html")
+
+
+        new_entry = cronjob(titel=titel,adresse=adresse,authentifizierung_checked=authentifizierung_checked,benutzername=benutzername,passwort=passwort,benachrichtigung_fehlschlag=benachrichtigung_fehlschlag,benachrichtigung_erfolg=benachrichtigung_erfolgnachfehl,benachrichtigung_deaktivierung=benachrichtigung_deaktivierung,antwort_speichern=antwort_speichern)
