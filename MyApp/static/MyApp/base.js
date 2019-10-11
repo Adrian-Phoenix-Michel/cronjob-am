b5cb711b1e56166953c0ac8b0c7bb39fe9cce8a1
adrian.michel <adrian.p.michel@hotmail.com>
2019-10-11 16:59:37 +0200

    Version 1.0 development

--- MyApp/static/MyApp/base.js
+++ MyApp/static/MyApp/base.js
@@ -17,6 +17,50 @@ function myFunction() {
     }
 }
 
+function myReset() {
+    var reset = document.getElementById("Reset");
+
+    var title = document.getElementById("Title");
+    var address = document.getElementById("Address");
+
+    var checkBox = document.getElementById("myCheck");
+    var text = document.getElementById("text");
+    var benutzerName = document.getElementById("Benutzername");
+    var passWort = document.getElementById("Passwort");
+
+    var input1 = document.getElementById("Input1");
+    var input2 = document.getElementById("Input2");
+    var input3 = document.getElementById("Input3");
+    var custom = document.getElementById("InputCustom");
+    var textarea = document.getElementById("myTextArea");
+
+    var inputbf = document.getElementById("InputBF");
+    var inputbe = document.getElementById("InputBE");
+    var inputbd = document.getElementById("InputBD");
+
+    var inputas = document.getElementById("InputAS");
+
+
+    benutzerName.value="";
+    passWort.value="";
+    title.value="";
+    address.value="https://";
+    checkBox.checked = false;
+    text.style.display = "none";
+    textarea.style.display = "none";
+    textarea.value="Your text here";
+    input1.checked = false;
+    input2.checked = false;
+    input3.checked = false;
+    custom.checked = false;
+    inputbf.checked = false;
+    inputbe.checked = false;
+    inputbd.checked = false;
+    inputas.checked = false;
+
+
+}
+
 function myCustom() {
     var custom = document.getElementById("InputCustom");
     var textarea = document.getElementById("myTextArea");
